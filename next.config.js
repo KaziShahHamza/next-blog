e66ae60137970c764df3aa4d78fc5f3327a9168d
soulfulDreamer <kazishahhamza@outlook.com>
2024-11-18 09:33:50 +0600

    comment added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["lh3.googleuserconent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
